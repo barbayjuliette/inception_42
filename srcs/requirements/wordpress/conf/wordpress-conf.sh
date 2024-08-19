b3a4e2c5ee9b4658e808966b06e169e78c979a75
Juliette Barbay <jbarbay@c2r3s5.42singapore.sg>
2024-08-19 17:43:30 +0800

    Changes db

--- srcs/requirements/wordpress/conf/wordpress-conf.sh
+++ srcs/requirements/wordpress/conf/wordpress-conf.sh
@@ -16,20 +16,28 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
 
+	sed -i 's/database_name_here/'"$DB_NAME"'/g' var/www/html/wp-config.php
+	sed -i 's/localhost/'"$DB_HOST"'/g' var/www/html/wp-config.php
+	sed -i 's/username_here/'"$DB_USER"'/g' var/www/html/wp-config.php
+	sed -i 's/password_here/'"$DB_PASSWORD"'/g' var/www/html/wp-config.php
 
-	echo "Setting up Wordpress..."
-	wp core download --path=/var/www/html --allow-root;
+	# Copy WordPress config file
+	cp var/www/html/wp-config-sample.php var/www/html/wp-config.php
 
-	echo "Creating wp-config.php"
-	wp config create --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root --path=/var/www/html
-	wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
-	# mv /var/www/wp-config.php /var/www/html
+	sleep 5
+	# echo "Setting up Wordpress..."
+	# wp core download --path=/var/www/html --allow-root;
+
+	# echo "Creating wp-config.php"
+	# wp config create --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root --path=/var/www/html
+	# wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
+	# # mv /var/www/wp-config.php /var/www/html
 
 	echo "Wordpress setting up DB"
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email 
+	wp core install --path=/var/www/html --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email 
 
 	echo "Wordpress: Creating users..."
-	wp user create --allow-root ${DB_USER} ${DB_EMAIL} --user_pass=${DB_PASSWORD};
+	wp user create --path=/var/www/html --allow-root ${DB_USER} ${DB_EMAIL} --user_pass=${DB_PASSWORD};
 	echo "Wordpress: set up!"
 fi
 
