7571f3a3915fc99df893a03fd405987b186cea66
Juliette Barbay <jbarbay@c2r3s5.42singapore.sg>
2024-08-19 13:21:53 +0800

    test

--- srcs/requirements/wordpress/conf/wordpress-conf.sh
+++ srcs/requirements/wordpress/conf/wordpress-conf.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-	# sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+	sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
 	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
+	chmod -R 755 /var/www/*;
 	mkdir -p /run/php/;
 	touch /run/php/php7.4-fpm.pid;
 
@@ -21,15 +21,14 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --path=/var/www/html --allow-root;
 
 	echo "Creating wp-config.php"
-	# mv /var/www/wp-config.php /var/www/html
 	wp config create --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root --path=/var/www/html
-	wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
+	# mv /var/www/wp-config.php /var/www/html
+	# wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
 
 	echo "Wordpress setting up DB"
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email 
 
 	echo "Wordpress: Creating users..."
-	echo "WP_USER_LOGIN: ${WP_USER_LOGIN}"
 	wp user create --allow-root ${DB_USER} ${DB_EMAIL} --user_pass=${DB_PASSWORD};
 	echo "Wordpress: set up!"
 fi
